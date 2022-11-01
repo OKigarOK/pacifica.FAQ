19a7627df40db2b65d9151c001582e81daee0e63
OKigarOK <91433816+OKigarOK@users.noreply.github.com>
2022-11-01 16:21:30 +0300

    Add files via upload

--- scripts/accordion.js
+++ scripts/accordion.js
@@ -2,6 +2,8 @@ const acc = document.getElementsByClassName("accordion");
 
 const allPanels = document.getElementsByClassName("panel")
 
+// console.log(allPanels);
+
 // for (let panelHeight of allPanels) {
 //   console.log(panelHeight);
 // }
@@ -10,13 +12,24 @@ for (let i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     this.classList.toggle("active");
     const panel = this.nextElementSibling;
-    // const parent = this.parentElement;
+
+    console.log(this.parentElement);
+
+    const parent = this.parentElement;
+    // console.log(parent);
+
     if (panel.style.maxHeight){
       panel.style.maxHeight = null;
     } else {
       // panel.style.maxHeight = panel.scrollHeight + "px";
-      panel.style.maxHeight = "100%";
-      // parent.style.maxHeight = "100%"
+
+      // parent.style.maxHeight = parent.scrollHeight + panel.scrollHeight + 'px'
+
+      // console.log(parent.style.maxHeight);
+
+
+      // panel.style.maxHeight = "100vh";
+      panel.style.maxHeight = "100%"
     } 
   });
 }
